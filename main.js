02370b607a003f0f17ead4e7e1bd817261a9f7cf
Joel Josy <joeljosy79@gmail.com>
2023-03-06 11:17:18 +0530

    update playround, check player=rock

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 function getComputerChoice() {
     return options[Math.floor(Math.random()*3)];
-}
+};
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection) {
-        console.log("It's a Tie!")
-    }
+        console.log("It's a Tie!");
+    };
+    elif (playerSelection.toLowerCase() == 'rock') {
+        switch (computerSelection) {
+            case 'paper':
+                console.log("You Lose! Paper beats Rock");
+                break;
+            case 'scissors':
+                console.log("You Win! Rock beats Scissors");
+                break;
+        };
+    };
 
-}
+};
 
-const options = ['rock', 'paper', 'scissors']
+const options = ['rock', 'paper', 'scissors'];
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
