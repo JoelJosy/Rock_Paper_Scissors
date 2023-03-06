2f9ab2b61e93d4d1b5e2bb6e178bfde246815f0a
Joel Josy <joeljosy79@gmail.com>
2023-03-06 11:24:26 +0530

    Finish fn playround

--- main.js
+++ main.js
@@ -5,14 +5,34 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection) {
         console.log("It's a Tie!");
-    };
-    elif (playerSelection.toLowerCase() == 'rock') {
+    }
+    else if (playerSelection.toLowerCase() == 'rock') {
         switch (computerSelection) {
             case 'paper':
                 console.log("You Lose! Paper beats Rock");
                 break;
             case 'scissors':
                 console.log("You Win! Rock beats Scissors");
+                break;
+        };
+    }
+    else if (playerSelection.toLowerCase() == 'paper') {
+        switch (computerSelection) {
+            case 'scissors':
+                console.log("You Lose! Scissors beats Paper");
+                break;
+            case 'rock':
+                console.log("You Win! Paper beats Rock");
+                break;
+        };
+    }
+    else if (playerSelection.toLowerCase() == 'scissors') {
+        switch (computerSelection) {
+            case 'rock':
+                console.log("You Lose! Rock beats Scissors");
+                break;
+            case 'paper':
+                console.log("You Win! Scissors beats Paper");
                 break;
         };
     };
@@ -23,3 +43,4 @@ const options = ['rock', 'paper', 'scissors'];
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection)
